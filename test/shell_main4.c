48755aaf357c55a7bd223e7cc02a59a8444fbdb7
Kyamah <kyamah10@gmail.com>
2024-08-26 07:06:25 -0700

    Shell

--- test/shell_main4.c
+++ test/shell_main4.c
@@ -1,21 +1,11 @@
 #include "shell.h"
-
-void run_cmd(char * * cmd, char *agv);
-
-/ * *
- *  handle_input - take input from stdin
- *  @agv: command line argument (executable file)
- *  @input_stream: input stream
- *  
- *  Return: void
- * /
+void run_cmd(char **cmd, char *agv);
 void handle_input(char *agv, FILE *input_stream)
 {
 char *input = NULL, *command, *TokPtr = NULL;
 size_t len = 0;
 int readLine, i;
 char *cmdV[ARG_MAX_VAL];
-
 while (1)
 {
 if (isatty(fileno(stdin)) && input_stream == stdin)
@@ -33,7 +23,6 @@ input[readLine - 1] = '\0';
 
 if (input == NULL || *input == '\n' || *input == '\t')
 continue;
-
 i = 0;
 command = strtok_r(input, " \n", &TokPtr);
 while (command != NULL && i < ARG_MAX_VAL - 1)
@@ -42,35 +31,22 @@ cmdV[i++] = command;
 command = strtok_r(NULL, " \n", &TokPtr);
 }
 cmdV[i] = NULL;
-
 if (cmdV[0] != NULL)
 run_cmd(cmdV, agv);
 }
-
 free(input);
 }
-
-/ * *
- *  run_cmd - run commands
- *  @cmd: double pointer to the command line args
- *  @agv: command line argument (compiled file)
- *  
- *  Return: void
- * /
-void run_cmd(char * * cmd, char *agv)
+void run_cmd(char **cmd, char *agv)
 {
 int path_found;
-char * * env = environ, path[PATH_MAX];
+char **env = environ, path[PATH_MAX];
 pid_t id;
-
-void ( * f)(char * * ) = is_builtin(cmd);
-
+void (*f)(char **) = is_builtin(cmd);
 if (f != NULL)
 {
 f(cmd);
 return;
 }
-
 path_found = find_path(cmd[0], path, sizeof(path));
 if (path_found != 1)
 {
@@ -93,14 +69,6 @@ wait(NULL);
 else
 perror("fork");
 }
-
-/ * *
- *  main - simple shell
- *  @argc: argument count
- *  @argv: argument vector
- *  
- *  Return: 0 on success
- * /
 int main(int argc, char *argv[])
 {
 FILE *input_stream = stdin;
@@ -120,30 +88,18 @@ if (input_stream != stdin)
 pclose(input_stream);
 return (EXIT_SUCCESS);
 }
-
-#include <stdlib.h> /* Include the standard library header for setenv and unsetenv */
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
-extern char * * environ; /* Declare the environment variable */
-
-/**
- *run_cmd - handle commands including built-in commands
- *@cmd : double pointer to the command line args
- *@agv : command line argument (compiled file)
- *  
- *  Return: void
- * /
-void run_cmd(char * * cmd, char *agv)
+extern char **environ;
+void run_cmd(char **cmd, char *agv)
 {
 int path_found;
-char * * env = environ, path[PATH_MAX];
+char **env = environ, path[PATH_MAX];
 pid_t id;
-
-/ *Check for built-in commands like setenv and unsetenv*/
 if (strcmp(cmd[0], "setenv") == 0)
 {
 if (cmd[1] && cmd[2])
@@ -159,7 +115,6 @@ dprintf(STDERR_FILENO, "Usage: setenv VARIABLE VALUE\n");
 }
 return;
 }
-
 if (strcmp(cmd[0], "unsetenv") == 0)
 {
 if (cmd[1])
@@ -175,16 +130,12 @@ dprintf(STDERR_FILENO, "Usage: unsetenv VARIABLE\n");
 }
 return;
 }
-
-/ *  Existing code for handling other commands */
-void (*f)(char * * ) = is_builtin(cmd);
-
+void (*f)(char **) = is_builtin(cmd);
 if (f != NULL)
 {
 f(cmd);
 return;
 }
-
 path_found = find_path(cmd[0], path, sizeof(path));
 if (path_found != 1)
 {
@@ -192,7 +143,6 @@ dprintf(STDERR_FILENO, "%s: No such file or directory\n", agv);
 return;
 }
 cmd[0] = path;
-
 id = fork();
 if (id == 0)
 {
